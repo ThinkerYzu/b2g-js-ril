0ccc6544cbb1ce4c14675ad734066e15f7fa2dcf
Philipp von Weitershausen <philipp@weitershausen.de>
2011-11-09 15:47:21 -0800

    Boilerplate for socket transport

--- bootstrap.js
+++ bootstrap.js
@@ -5,17 +5,25 @@ Cu.import("resource://gre/modules/AddonManager.jsm");
 
 const GLOBAL_SCOPE = this;
 
+let listener;
+
 function startup(data, reason) {
   function load(file) {
     Services.scriptloader.loadSubScript(data.resourceURI.spec + file,
                                         GLOBAL_SCOPE);
   }
   load("src/Parcel.js");
+  load("socket.js");
+  listener = new SocketListener();
+  //listener.listen("host", "port");
 }
 
 
 function shutdown(data, reason) {
-  // Easily reload scripts: auto-enable when disabling
+  listener.stop();
+
+  // Re-enable the ourselves when we get disabled. That way you can reload this
+  // code by simply clicking the "Disable" button in about:addons.
   AddonManager.getAddonByID(data.id, function(addon) {
     addon.userDisabled = false;
   });
